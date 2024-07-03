e2b5ebfe8692ecc5162ddd961491e123449bfda7
tamarin <tamarin500k@gmail.com>
2024-07-03 14:45:04 +0300

    fix: deleted cdn link from index.html and added import for bootstrap.min.css in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,7 @@
  */
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { App } from "./App";
